c5993c051cbb25557c84a121ad72c7c64b4a638d
TorrieL <hmminicity2@163.com>
2019-08-03 18:02:19 +0800

    tabbed content

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Tooltip from './rookie-ui/tooltip'
 import Dropdown from './rookie-ui/dropdown'
+import Tabs from './rookie-ui/tabs'
 
 //create a Tooltip instance
 const tooltip =new Tooltip(document.querySelector('.tooltip'));
@@ -12,3 +13,7 @@ dropdowns.forEach(dropdown => {
   const instance = new Dropdown(dropdown);
   instance.init();
 });
+
+//create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
---
+++ src/rookie-ui/tabs.js
@@ -0,0 +1,31 @@
+import './styles/tabs.css'
+class Tabs {
+  constructor(container) {
+    this.container = container;
+    this.tabs = container.querySelectorAll('.trigger');
+  }
+  init(){
+    this.tabs.forEach(tab => {
+      tab.addEventListener('click', e => {
+        this.toggleTabs(e);
+        this.toggleContent(e);
+      })
+    })
+  }
+  toggleTabs(e){
+    //remove current active class
+    this.tabs.forEach(tab => tab.classList.remove('active'));
+    //add new active class
+    e.target.classList.add('active');
+  }
+  toggleContent(e){
+    //remove current active classes from content
+    this.container.querySelectorAll('.content').forEach(item => item.classList.remove('active'));
+    //add new active class to content
+    const selector = e.target.getAttribute('data-target');
+    const content = this.container.querySelector(selector);
+    content.classList.add('active');
+  }
+}
+
+export{ Tabs as default };
