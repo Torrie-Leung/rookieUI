5445d44ea450db90fbf19ffd8622af7f0c150305
TorrieL <hmminicity2@163.com>
2019-08-03 10:59:58 +0800

    dropdown

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import Tooltip from './rookie-ui/tooltip'
-
+import Dropdown from './rookie-ui/dropdown'
 
 //create a Tooltip instance
 const tooltip =new Tooltip(document.querySelector('.tooltip'));
 tooltip.init();
+
+//create Dropdown instances
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+  const instance = new Dropdown(dropdown);
+  instance.init();
+});
